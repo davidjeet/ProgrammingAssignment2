79de86a0983a849b36c6f391990046ea02f33eeb
David Green <davidgreen24@gmail.com>
2015-06-19 15:22:57 -0400

    Removing references to makeVector and cachemean

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 ## Write a short comment describing this function
 
+
+# To create a sample matrix, do some thing like this:
+# t <- matrix(rnorm(9),3,3)   
+
+
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -34,40 +39,3 @@ cacheSolve <- function(x, ...) {
         message("...and placing the result in the cache")
         m
 }
-
-
-t <- matrix(rnorm(9),3,3)   
-
-
-######################################################################################################
-
-
-
-
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
-
-
-cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
-}
-
